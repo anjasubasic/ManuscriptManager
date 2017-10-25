041a4f5f737343612169ab632870d82a103e48f6
Marissa Le Coz <Marissa.C.Le.Coz.17@dartmouth.edu>
2017-10-24 22:14:46 -0400

    editor registration

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 # User Codes
 NOT_LOGGED_IN = 0
 EDITOR = 1
@@ -12,8 +14,15 @@ while True:
 
     command = raw_input("ManuscriptManager> ")
 
-    if USER_CODE == NOT_LOGGED_IN and command[0:15] == "register editor": # this is sort of hacky, but..
-        print("hi")
+    if USER_CODE == NOT_LOGGED_IN and command[0:15] == "register editor":
+        word_list = command.split() # word list can be indexed into to get each word in the command
+        word_count = len(word_list)
+        if word_count != 4:
+            print("Please use the format 'register editor <first name> <last name>")
+        else:
+            # add to database editor: first name, last name, id (which is a global)
+            print("Welcome! Your id for login is " + str(id) + ".")
+            id = id + 1 # increment the global id variable so that we always have unique id's
 
     elif USER_CODE == NOT_LOGGED_IN and command[0:15] == "register author":
         print("hi")
@@ -25,6 +34,8 @@ while True:
 
     else:
         print("Invalid command.")
+
+
 
 
 
