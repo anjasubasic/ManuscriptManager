b2031c04f35a2b45b388a3d2670ef8641de413e1
Marissa Le Coz <Marissa.C.Le.Coz.17@dartmouth.edu>
2017-10-24 21:57:30 -0400

    rough skeleton

--- app.py
+++ app.py
@@ -1,1 +1,34 @@
-print("hi")
+# User Codes
+NOT_LOGGED_IN = 0
+EDITOR = 1
+AUTHOR = 2
+REVIEWER = 3
+
+USER_CODE = NOT_LOGGED_IN;
+id = 1;
+
+
+while True:
+
+    command = raw_input("ManuscriptManager> ")
+
+    if USER_CODE == NOT_LOGGED_IN and command[0:15] == "register editor": # this is sort of hacky, but..
+        print("hi")
+
+    elif USER_CODE == NOT_LOGGED_IN and command[0:15] == "register author":
+        print("hi")
+
+    elif USER_CODE == NOT_LOGGED_IN and command[0:17] == "register reviewer":
+        print("hi")
+
+    # and so on and so forth
+
+    else:
+        print("Invalid command.")
+
+
+
+
+
+
+
