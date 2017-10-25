72f3ea0acd673bda6aefe4689d42b438e5a22e20
Anja Subasic <anja.subasic@gmail.com>
2017-10-25 16:48:52 -0400

    Converted harcoded RI codes into a loop to allow for different number of interests

--- app.py
+++ app.py
@@ -82,7 +82,6 @@ while True:
         ri_codes = raw_input("ManuscriptManager> Enter 1 to 3 RICodes separated by the '/' character: ")
         ri_codes_list = ri_codes.split('/')
         num_of_entered_ri_codes = len(ri_codes_list)
-        print(num_of_entered_ri_codes)
 
         if first_name == "" or last_name == "" or ri_codes == "":
             print("Registration failed. Make sure you enter a value for each field.")
@@ -92,7 +91,6 @@ while True:
             print("Registration failed. Make sure you enter valid RI codes.")
 
         else:
-
             connection = mysql.connector.connect(user=username, password=password, host=host, database=database)
             cursor = connection.cursor(buffered=True)
             # Gets number of valid interests:
@@ -102,13 +100,13 @@ while True:
                 cursor.execute(validate_interests_query)
                 if cursor.fetchone()[0] == 1:
                     num_of_valid_codes = num_of_valid_codes + 1
-                connection.close()
+
+            connection.close()
 
             if num_of_valid_codes != num_of_entered_ri_codes:
                 print("Registration failed. You have entered one or more invalid RICodes.")
 
             else:
-
                 connection = mysql.connector.connect(user=username, password=password, host=host, database=database)
                 cursor = connection.cursor(buffered=True)
                 # Add reviewer
@@ -119,16 +117,34 @@ while True:
 
                 idReviewer = cursor.lastrowid
 
+                formattedRICodeList = ""
+
+                for idx, code in enumerate(ri_codes_list):
+                    formattedRICodeList = formattedRICodeList + "\"" + code + "\""
+                    if idx < len(ri_codes_list) - 1:
+                        formattedRICodeList = formattedRICodeList + ","
+
+
                 # Get RICode ids for selected interests
-                get_ricode_ids_query = "SELECT idRICode FROM ricode WHERE RIValue IN (\"" + ri_code1 + "\", \"" + ri_code2 + "\", \"" + ri_code3 + "\")"
+                get_ricode_ids_query = "SELECT idRICode FROM ricode WHERE RIValue IN (" + formattedRICodeList + ")"
                 cursor.execute(get_ricode_ids_query)
 
                 ri_codes = [item[0] for item in cursor.fetchall()] # Gets all three RI Codes. cursor.fetchall() returns tuples, this only gets the value of each item
 
+                # Format input for interest table
+
+                formatted_input_list = ""
+
+
+                # (1,102), (1,103)
+
+                for idx, code in enumerate(ri_codes):
+                    formatted_input_list = formatted_input_list + "(" + str(idReviewer) + "," + str(code) + ")"
+                    if idx < len(ri_codes) - 1:
+                        formatted_input_list = formatted_input_list + ","
+
                 # Add to interest table
-                add_interest_query = "INSERT INTO interest VALUES (\"" + str(idReviewer) + "\", \"" + str(ri_codes[0]) + "\"), " \
-                                                            " (\"" + str(idReviewer) + "\", \"" + str(ri_codes[1]) + "\"), " \
-                                                            " (\"" + str(idReviewer) + "\", \"" + str(ri_codes[2]) + "\") "
+                add_interest_query = "INSERT INTO interest VALUES " + formatted_input_list
                 cursor.execute(add_interest_query)
 
                 connection.commit()
